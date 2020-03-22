4f230b93194146c2792edaa744957efd643ae3e8
Michał Wołoszyn <fraunos@gmail.com>
2020-03-22 02:24:14 +0100

    beautify

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const Koa = require('koa')
 const IO = require('koa-socket-2')
 const ytdl = require('youtube-dl')
@@ -9,91 +8,101 @@ var idCounter = 0
 var timeAvailable = null
 
 async function start() {
-  const app = new Koa()
-  const io = new IO()
-  const host = process.env.HOST || '0.0.0.0'
-  const port = process.env.PORT || 3000
-  
-  let usersCount = 0;
+    const app = new Koa()
+    const io = new IO()
+    const host = process.env.HOST || '0.0.0.0'
+    const port = process.env.PORT || 3000
 
-  app.use(async (ctx, next) => {
-    // await next()
-    ctx.type = 'html'
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    ctx.body = fs.readFileSync('index.html', {'encoding': 'utf8'})
+    let usersCount = 0;
 
-    // return new Promise((resolve, reject) => {
-    //   ctx.res.on('close', resolve)
-    //   ctx.res.on('finish', resolve)
-    // })
-  })
-  io.attach(app)
-  
-  io.use(async (ctx, next)=> {
-      console.log(ctx);
-      await next()
-  })
+    app.use(async (ctx, next) => {
+        // await next()
+        ctx.type = 'html'
+        ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+        ctx.body = fs.readFileSync('index.html', {
+            'encoding': 'utf8'
+        })
 
-  io.on('connect', ctx => {
-    console.log('a user connected')
-    usersCount++;
-    console.log(usersCount);
-    io.broadcast('updateUserCount', usersCount)
-    
-    if (ctx.emit) {
-        console.log("dupa");
-        ctx.emit('updatePlaylist', playlist)
-        ctx.emit('updateUserCount', usersCount)
-    }
-  })
-  
-  
-    io.on('disconnect', (ctx, data) => {
-      console.log('a user disconnected')
-      usersCount--;
-      io.broadcast('updateUserCount', usersCount)
-      
-      console.log(ctx.socket);
-      if (ctx.socket) {
-          ctx.socket.volatile.emit('updatePlaylist', playlist)
-      }
+        // return new Promise((resolve, reject) => {
+        //   ctx.res.on('close', resolve)
+        //   ctx.res.on('finish', resolve)
+        // })
     })
-  io.on('sendPlaylistItem', async function (ctx, data) {
-    console.log(data)
-    let info = await getInfo(data)
+    io.attach(app)
 
-    if (info.length>1) {
-      for (let trackInfo of info) {
-        playlist.push(createPlaylistItem(trackInfo))
-      }
-    } else {
-      playlist.push(createPlaylistItem(info))
-    }
-    console.log(playlist)
-    io.broadcast('updatePlaylist', playlist)
-  })
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+    io.use(async (ctx, next) => {
+        console.log(ctx);
+        await next()
+    })
+
+    io.on('connect', ctx => {
+        console.log('a user connected')
+        usersCount++;
+        console.log(usersCount);
+        io.broadcast('updateUserCount', usersCount)
+
+        if (ctx.emit) {
+            console.log("dupa");
+            ctx.emit('updatePlaylist', playlist)
+            ctx.emit('updateUserCount', usersCount)
+        }
+    })
+
+
+    io.on('disconnect', (ctx, data) => {
+        console.log('a user disconnected')
+        usersCount--;
+        io.broadcast('updateUserCount', usersCount)
+
+        console.log(ctx.socket);
+        if (ctx.socket) {
+            ctx.socket.volatile.emit('updatePlaylist', playlist)
+        }
+    })
+    io.on('sendPlaylistItem', async function(ctx, data) {
+        console.log(data)
+        let info = await getInfo(data)
+
+        if (info.length > 1) {
+            for (let trackInfo of info) {
+                playlist.push(createPlaylistItem(trackInfo))
+            }
+        } else {
+            playlist.push(createPlaylistItem(info))
+        }
+        console.log(playlist)
+        io.broadcast('updatePlaylist', playlist)
+    })
+    app.listen(port, host)
+    console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
 
 start()
 
 function getInfo(data) {
-  return new Promise((resolve, reject) => {
-    ytdl.getInfo(data, function (err, info) {
-      console.log(info);
-      resolve(info)
+    return new Promise((resolve, reject) => {
+        ytdl.getInfo(data, function(err, info) {
+            console.log(info);
+            resolve(info)
+        })
     })
-  })
 }
+
 function createPlaylistItem(info) {
-  let timestamp = new Date().getTime()
-  let playTime
-  if (timeAvailable > timestamp) {
-      playTime = timeAvailable
-  } else {
-    playTime = timestamp
-  }
-  timeAvailable = playTime + info._duration_raw * 1000
-  return {start: 0, info: info, timestamp: timestamp, playTime: playTime, endTime: timeAvailable, id: idCounter++}
+    let timestamp = new Date().getTime()
+    let playTime
+    if (timeAvailable > timestamp) {
+        playTime = timeAvailable
+    } else {
+        playTime = timestamp
+    }
+    timeAvailable = playTime + info._duration_raw * 1000
+    return {
+        start: 0,
+        info: info,
+        timestamp: timestamp,
+        playTime: playTime,
+        endTime: timeAvailable,
+        id: idCounter++
+    }
 }
