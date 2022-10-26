35694c3c90b2a51079f4ad27b86fd0a2e9d8a94f
Michał Wołoszyn <fraunos@gmail.com>
2022-10-26 23:33:41 +0200

    improvements: - update deps - ui rework - shuffled playlist

--- server.js
+++ server.js
@@ -1,125 +1,134 @@
 const Koa = require('koa')
 const IO = require('koa-socket-2')
-const ytdl = require('youtube-dl')
+const ytdl = require('youtube-dl-exec')
 const fs = require('fs')
 
-var playlist = []
 var idCounter = 0
 var timeAvailable = null
+let users = [];
 
 async function start() {
-    const app = new Koa()
-    const io = new IO()
-    const host = process.env.HOST || '0.0.0.0'
-    const port = process.env.PORT || 3000
+  const app = new Koa()
+  const io = new IO()
+  const host = process.env.HOST || '0.0.0.0'
+  const port = process.env.PORT || 3000
 
-    let usersCount = 0;
 
-    app.use(async ctx => {
-        switch (ctx.originalUrl) {
-            case '/':
-                ctx.type = 'html'
-                ctx.status = 200
-                ctx.body = fs.readFileSync('index.html', {
-                    'encoding': 'utf8'
-                })
-                break;
-            case '/ping':
-                ctx.type = 'html'
-                ctx.status = 200
-                console.log('pong');
-                ctx.body = 'pong'
-                break;
-            default:
-                
-        }
-        // await next()
+  app.use(async ctx => {
+    if (ctx.originalUrl === "/") {
+      ctx.type = 'html'
+      ctx.status = 200
+      ctx.body = fs.readFileSync('index.html', {
+        'encoding': 'utf8'
+      })
 
-        // return new Promise((resolve, reject) => {
-        //   ctx.res.on('close', resolve)
-        //   ctx.res.on('finish', resolve)
-        // })
+    }
+  })
+  io.attach(app)
+
+
+  io.on('connect', ctx => {
+    console.log(`a user connected ${ctx.id}`)
+    const user = { id: ctx.id, connected: true, playlist: [], color: randomColor() }
+    users.push(user)
+    io.broadcast('updateUserCount', getConnectedUsers().length)
+    console.log(users)
+
+    if (ctx.emit) {
+      ctx.emit('updatePlaylist', shufflePlaylist())
+      ctx.emit('updateUserCount', getConnectedUsers().length)
+      ctx.emit('updateUserColor', user.color)
+    }
+  })
+
+
+  io.on('disconnect', (ctx, data) => {
+    console.log(`a user disconnected ${ctx.socket.id}`)
+    users.find(i => i.id === ctx.socket.id).connected = false;
+    io.broadcast('updateUserCount', getConnectedUsers().length)
+
+  })
+
+  setInterval(function() {
+    io.broadcast('timesync', new Date())
+  }, 1000);
+
+  io.on('sendPlaylistItem', async function(ctx, data) {
+    console.log(`user ${ctx.socket.id} sent ${data}`)
+    let info = await ytdl(data, {
+      dumpSingleJson: true,
+      noCheckCertificates: true,
+      noWarnings: true,
+      format: ['ba'],
+      addHeader: [
+        'referer:youtube.com',
+        'user-agent:googlebot'
+      ]
     })
-    io.attach(app)
 
-    io.use(async (ctx, next) => {
-        console.log(ctx);
-        await next()
-    })
-
-    io.on('connect', ctx => {
-        console.log('a user connected')
-        usersCount++;
-        console.log(usersCount);
-        io.broadcast('updateUserCount', usersCount)
-
-        if (ctx.emit) {
-            console.log("dupa");
-            ctx.emit('updatePlaylist', playlist)
-            ctx.emit('updateUserCount', usersCount)
-        }
-    })
-
-
-    io.on('disconnect', (ctx, data) => {
-        console.log('a user disconnected')
-        usersCount--;
-        io.broadcast('updateUserCount', usersCount)
-
-        console.log(ctx.socket);
-        if (ctx.socket) {
-            ctx.socket.volatile.emit('updatePlaylist', playlist)
-        }
-    })
-    
-    setInterval(function () {
-        io.broadcast('timesync', new Date())
-    }, 1000);
-    
-    io.on('sendPlaylistItem', async function(ctx, data) {
-        console.log(data)
-        let info = await getInfo(data)
-
-        if (info.length > 1) {
-            for (let trackInfo of info) {
-                playlist.push(createPlaylistItem(trackInfo))
-            }
-        } else {
-            playlist.push(createPlaylistItem(info))
-        }
-        console.log(playlist)
-        io.broadcast('updatePlaylist', playlist)
-    })
-    app.listen(port, host)
-    console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
+    console.log(getUser(ctx.socket.id))
+    if (info.length > 1) {
+      for (let trackInfo of info) {
+        getUser(ctx.socket.id).playlist.push(createPlaylistItem(ctx.socket.id, trackInfo))
+      }
+    } else {
+      getUser(ctx.socket.id).playlist.push(createPlaylistItem(ctx.socket.id, info))
+    }
+    console.log(users)
+    io.broadcast('updatePlaylist', shufflePlaylist())
+  })
+  app.listen(port, host)
+  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
 
 start()
 
-function getInfo(data) {
-    return new Promise((resolve, reject) => {
-        ytdl.getInfo(data, function(err, info) {
-            console.log(info);
-            resolve(info)
-        })
-    })
+function createPlaylistItem(user, info) {
+  const { title, requested_downloads, original_url, thumbnail, duration, duration_string } = info
+  let timestamp = new Date().getTime()
+  console.log(timestamp)
+  let playTime
+  if (timeAvailable > timestamp) {
+    playTime = timeAvailable
+  } else {
+    playTime = timestamp
+  }
+  timeAvailable = playTime + duration * 1000
+  return {
+    title,
+    url: requested_downloads[0].url,
+    color: getUser(user).color,
+    original_url,
+    thumbnail,
+    timestamp,
+    playTime,
+    duration,
+    duration_string,
+    endTime: timeAvailable,
+    id: idCounter++
+  }
 }
 
-function createPlaylistItem(info) {
-    let timestamp = new Date().getTime()
-    let playTime
-    if (timeAvailable > timestamp) {
-        playTime = timeAvailable
-    } else {
-        playTime = timestamp
+function getConnectedUsers() {
+  return users.filter(i => i.connected)
+}
+function getUser(id) {
+  return users.find(i => i.id === id)
+}
+function shufflePlaylist() {
+  const maxListLength = users.map(i => i.playlist.length).sort().pop()
+  const playlists = users.map(i => i.playlist)
+  let playlist = []
+  for (let i = 0; i < maxListLength; i++) {
+    for (let userPlaylist of playlists) {
+      userPlaylist[i] && playlist.push(userPlaylist[i])
     }
-    timeAvailable = playTime + info._duration_raw * 1000
-    return {
-        start: 0,
-        info: info,
-        timestamp: timestamp,
-        playTime: playTime,
-        endTime: timeAvailable,
-        id: idCounter++
-    }
+
+
+  }
+  console.log()
+  return playlist
+}
+function randomColor() {
+  return "#" + (Math.floor(Math.random() * 16777215).toString(16));
 }
